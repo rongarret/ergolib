babdb72fd39bdf288c9203f3159fa0c7fb5f203d
sabu36 <>
2020-12-23 22:32:14 +0900

    Rename sforce into strforce; Add sforce, collecting into a set

--- core/iterators.lisp
+++ core/iterators.lisp
@@ -201,6 +201,10 @@
     (for item in iterator do (collect item))))
 
 (defun sforce (iterator)
+  (with-scollector collect
+    (for item in iterator do (collect item))))
+
+(defun strforce (iterator)
   (with-char-collector collect
     (for item in iterator do (collect item))))
 
